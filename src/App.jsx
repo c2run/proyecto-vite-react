03ffc4c230ab993d175471070cb57f63de0057d9
c2run <jose.cerda66@gmail.com>
2023-04-08 11:04:23 -0400

    componente de clase

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Parser from "html-react-parser";
 import Basicos from "./componentes/Basicos";
+import ComponenteDeClase from "./componentes/ComponenteDeClase";
 
 function App() {
   return (
   <div>
    <h1>Hola</h1>
    <Basicos></Basicos>
+   <ComponenteDeClase></ComponenteDeClase>
   </div>
   )
 }
---
+++ src/componentes/ComponenteDeClase.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class ComponenteDeClase extends React.Component
+{
+    render(){
+        return (
+            <>
+                <h1>Hola de componente de clase</h1>
+            </>
+        )
+    }
+}
