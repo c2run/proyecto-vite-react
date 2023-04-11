50a86bfc67616619875659d1d4fb4e594cb853b4
c2run <jose.cerda66@gmail.com>
2023-04-11 11:49:37 -0400

    align center div container

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './../public/css/blog.css'
+import './index.css';
 
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
