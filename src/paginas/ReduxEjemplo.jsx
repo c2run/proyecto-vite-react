03f89dfba3df536807bfce935b3a807dffe2adcc
c2run <jose.cerda66@gmail.com>
2023-04-25 12:04:37 -0400

    Calculadora Reducer, redux toolkit ejemplo

--- src/paginas/ReduxEjemplo.jsx
+++ src/paginas/ReduxEjemplo.jsx
@@ -2,10 +2,16 @@ import { Link } from "react-router-dom"
 import { useSelector, useDispatch } from "react-redux"
 import {} from './../redux/features/procedenciaSlice'
 import { cambiarMexico,cambiarPeru,cambiarVenezuela, volverChile } from "./../redux/features/procedenciaSlice"
+import calculadoraSlice, {sumar, restar,multiplicar,dividir, resetear} from './../redux/features/calculadoraSlice'
+
 const ReduxEjemplo = () => {
     // const pais = useSelector((state) => state.procedencia.pais);
 
     const procedencia = useSelector((state) => state.procedencia);
+    
+    const calculadora = useSelector((state) => state.calculadora);
+
+
     const dispatch = useDispatch();
 
   return (
@@ -23,6 +29,23 @@ const ReduxEjemplo = () => {
         <button className="btn btn-danger" onClick={()=> dispatch(cambiarPeru())}>Cambiar a Peru</button>
         <hr />
         <button className="btn btn-danger" onClick={()=> dispatch(cambiarVenezuela())}>Cambiar a Venezuela</button>
+        <hr />
+        <h1>Ejemplo 2 de redux</h1>
+        <ul>
+          <li>Numero1: { calculadora.numero1 }</li>
+          <li>Numero2: {calculadora.numero2}</li>
+          <li>resultado: {calculadora.resultado}</li>
+        </ul>
+        <hr />
+        <button className="btn btn-danger" onClick={()=> dispatch(sumar())}>sumar</button>
+        <hr />
+        <button className="btn btn-primary" onClick={()=> dispatch(restar())}>restar</button>
+        <hr />
+        <button className="btn btn-danger" onClick={()=> dispatch(multiplicar())}>Multiplicar</button>
+        <hr />
+        <button className="btn btn-primary" onClick={()=> dispatch(dividir())}>dividir</button>
+        <hr />
+        <button className="btn btn-primary" onClick={()=> dispatch(resetear())}>resetear</button>
     </>
   )
 }
---
+++ src/redux/features/calculadoraSlice.jsx
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    numero1: 10,
+    numero2: 10,
+    resultado: 0,
+}
+
+export const calculadoraSlice = createSlice({
+    name: "calculadora",
+    initialState,
+    reducers:{
+        resetear: (state) => { state.resultado=0;  },
+        sumar: (state) => 
+        {
+            state.resultado = state.numero1 + state.numero2
+        },
+        restar: (state) => 
+        {
+            state.resultado = state.numero1 - state.numero2
+        },
+        multiplicar: (state) => 
+        {
+            state.resultado = state.numero1 * state.numero2
+        },
+        dividir: (state) => 
+        {
+            if(state.numero1 !=0)
+            {
+                state.resultado = state.numero1 / state.numero2
+            }else
+            {
+                state.resultado=0;
+            }
+        },
+       
+           
+    }
+});
+
+export const {sumar, restar, multiplicar, dividir, resetear, mostrarNombre} = calculadoraSlice.actions
+export default calculadoraSlice.reducer
